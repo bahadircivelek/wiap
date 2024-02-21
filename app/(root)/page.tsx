75cc32d7339c94cf41073285f85cfc935556264d
bahadircivelek <bahadir.civelek@ngteknoloji.com>
2024-02-21 18:25:46 +0300

    pagination and search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,26 @@
+import CategoryFilter from "@/components/shared/CategoryFilter";
 import Collection from "@/components/shared/Collection";
+import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
 import { getAllEvents } from "@/lib/actions/event.action";
+import { SearchParamProps } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
-import { describe } from "node:test";
 
-export default async function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || '';
+  const category = (searchParams?.category as string) || '';
+
 
   const events = await getAllEvents({
-    query: '',
-    category: '',
-    page: 1,
-    limit: 6
+    query: searchText,
+    category: category,
+    page: page,
+    limit: 2
   });
-
+  console.log(events);
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -41,17 +48,17 @@ export default async function Home() {
       <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
         <h2 className="h2-bold">Trust by <br /> Thausands of Events</h2>
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          Search
-          CategoryFilter
+          <Search />
+          <CategoryFilter />
         </div>
         <Collection
           data={events?.data}
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
           collectionType="All_Events"
-          limit={6}
-          page={1}
-          totalPages={2} />
+          limit={3}
+          page={page}
+          totalPages={events?.totalPages} />
       </section>
     </>
   );
